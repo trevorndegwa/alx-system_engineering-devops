181e357b2900e124d1f1318bc9e41643a2c1a831
trevor ndegwa <tntrende@gmail.com>
2024-08-13 10:42:25 +0300

    queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function requests from Reddit API to return the
+number of subscribers for a given subreddit passed as bash arg"""
+
+import requests
+
+def number_of_subscribers(subreddit)
+    """function queries the Reddit API and then returns the number of subscribers for a particular subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
