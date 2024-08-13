89dea351d225c505084e331c532ac2b4805f3975
trevor ndegwa <tntrende@gmail.com>
2024-08-13 12:20:56 +0300

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """function requests from Reddit API to return the
-number of subscribers for a given subreddit passed as bash arg"""
+number of subscribers for a given subreddit"""
 
 import requests
+
 
 def number_of_subscribers(subreddit):
     """function queries the Reddit API and then returns the number of subscribers for a particular subreddit"""
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {
+            'User-Agent': 'My User Agent 1.0'
+            }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
