8b1f699ba5e3d2207a1a53fa68d114befb76d178
trevor ndegwa <tntrende@gmail.com>
2024-08-13 12:33:48 +0300

    compliance with PEP-8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """function queries the Reddit API and then returns the number of 
+    """function queries the Reddit API and then returns the number of
     subscribers for a particular subreddit"""
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
