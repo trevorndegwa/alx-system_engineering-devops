ae0e85af8d154068989b75cb0e49a2d52a89de9a
trevorndegwa <tntrende@gmail.com>
2024-08-23 03:44:29 +0300

    advanced web debugging 4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 
 # Increases hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/lmits.conf'
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
 }
 
